0c2b6aec4cb5eb827b4e84ee39633010568e3437
Ana Elisa <elisaibz92@gmail.com>
2025-01-22 10:48:22 +0100

    template

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <div>
+      <h1>Template</h1>
+      <p>Mi primera web de React :)</p>
+    </div>
+  );
+}
+
+export default App;
